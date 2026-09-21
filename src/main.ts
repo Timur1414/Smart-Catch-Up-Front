2024605d8133ac3206dde3bb1cd5a01311eb0961
Timur <tima.temirov.2005@mail.ru>
2026-09-21 22:05:33 +0300

    base menu (don't rerender).

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import './style.css'
 import {Router} from "./router/router.ts";
 import {HomePage} from "./pages/Home/home.ts";
+import {MainMenu} from "./components/MainMenu/main_menu.ts";
 
-const root: HTMLElement = document.getElementById('app') as HTMLElement;
+const root: HTMLElement = document.getElementById("app") as HTMLElement;
 export const router = new Router(root);
 
 router.addRoute("/", () => new HomePage());
 
 async function init(): Promise<void> {
+    let menu = new MainMenu({});
+    let menu_root: HTMLElement = document.getElementById("menu")!;
+    menu.render(menu_root);
+
     router.start();
 }
 
--- src/pages/Home/home.ts
+++ src/pages/Home/home.ts
@@ -2,17 +2,11 @@ import {BasePage} from "../base_page.ts";
 import template from "./home.hbs?raw";
 import "./home.css";
 import Handlebars from "handlebars";
-import {MainMenu} from "../../components/MainMenu/main_menu.ts";
 
 export class HomePage extends BasePage {
     async render(root: HTMLElement): Promise<void> {
         let compiledTemplate = Handlebars.compile(template);
         let html = compiledTemplate({}).trim();
         root.innerHTML = html;
-        let menu = new MainMenu({});
-        let menu_root =root.querySelector<HTMLElement>(".main-container");
-        if (!menu_root)
-            return;
-        menu.render(menu_root);
     }
 }
