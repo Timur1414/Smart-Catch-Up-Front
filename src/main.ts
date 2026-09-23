178e2fed21de817ebf32b0050b55d68589d5108c
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2026-09-24 00:50:18 +0300

    login page.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import {Router} from "./router/router.ts";
 import {HomePage} from "./pages/Home/home.ts";
 import {MainMenu} from "./components/MainMenu/main_menu.ts";
 import {NotFoundPage} from "./pages/NotFound/not_found.ts";
+import {LoginPage} from "./pages/Login/login.ts";
 
 const root: HTMLElement = document.getElementById("app") as HTMLElement;
 export const router = new Router(root);
 
 router.addRoute("/", () => new HomePage())
+    .addRoute("/login", () => new LoginPage())
     .addRoute("/404", () => new NotFoundPage());
 
 async function init(): Promise<void> {
